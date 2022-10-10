0e43d962a354d4c04338968bf22c883df86db13e
yuya <yattarude76@gmail.com>
2022-10-10 14:29:05 +0900

    from picture to map

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ from tkinter import Image
 import streamlit as st
 import numpy as np
 import pandas as pd
-from PIL import Image
 
 st.title('Streamlit 超入門')
 
 st.write('DataFrame')
 
-img = Image.open('IMG_3102.jpg')
-st.image(img, caption='field map', use_column_width=True)
+df = pd.DataFrame(
+    np.random.rand(100,2)/[5,5] + [43.060, 141.354],
+    columns=['lat', 'lon']
+)
 
-# df = pd.DataFrame(
-#     np.random.rand(100,2)/[5,5] + [43.060, 141.354],
-#     columns=['lat', 'lon']
-# )
+st.map(df)
 
-# st.map(df)
-
-#st.table(df.style.highlight_max(axis=1))
+st.table(df.style.highlight_max(axis=1))
